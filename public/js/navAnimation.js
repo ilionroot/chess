364904fd1da9d6e56bc853a02084e55655d3615a
Ctorum <vhcabralrezende@hotmail.com>
2020-06-09 20:08:19 -0300

    upload do igoncio

---
+++ public/js/navAnimation.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-linker');
+    const navLinks = document.querySelectorAll('.nav-linker li');
+
+    //Toggle Navigator
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        
+    //Animate Links
+
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkAnimation 0.5s ease forwards ${index / 7 + 1}s`;
+                }
+            });
+           //Burger Animation
+
+           burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
