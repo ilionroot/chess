12fef272f0c2fc73efc24784dffe6cfb85144f66
ilionroot <65546863+igormelo2207@users.noreply.github.com>
2020-06-09 17:02:31 -0300

    v1

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// Configs
+    app.use(express.static(path.join(__dirname, "public")))
 // Rotas
     app.get('/', (req, res) => {
-        res.sendFile(__dirname + "/chat.html");
+        res.sendFile(__dirname + "/public/chat.html");
+    })
+
+    let messages = [];
+
+    io.on('connection', socket => {
+
+        socket.emit('previousMessages', messages);
+
+        socket.on('sendMessage', function(data) {
+            messages.push(data);
+
+            socket.broadcast.emit('receivedMessage', data);
+        });
+
+        socket.on('disconnect', function() {
+            console.log('user ' + socket.id + " desconectado!");
+        })
+    })
+
+    app.post('/', (req, res) => {
+
     })
 
 server.listen(3000, () => {
