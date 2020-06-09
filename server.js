ca83eab8500df66e1f39fdb3e73d1527f54280ac
ilionroot <65546863+igormelo2207@users.noreply.github.com>
2020-06-09 13:54:00 -0300

    atualização pro vitoncio

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const net = require('net');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const handleConnection = socket => {
-    console.log('alguem se conectou');
-};
+// Rotas
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + "/chat.html");
+    })
 
-const server = net.createServer(handleConnection);
-server.listenerCount(3000, 'localhost');
+server.listen(3000, () => {
+    console.log('Server rodando!');
+});
